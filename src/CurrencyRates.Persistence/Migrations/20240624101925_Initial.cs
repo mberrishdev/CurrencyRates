bbaf17f4da644c08072f2c3b164657aa300659f5
Mikheil <84536014+mberrishdev@users.noreply.github.com>
2024-06-24 14:19:49 +0400

    Migrations

---
+++ src/CurrencyRates.Persistence/Migrations/20240624101925_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyRates.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrencyFrom = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    CurrencyTo = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+        }
+    }
+}
--- src/CurrencyRates.Persistence/obj/Debug/net8.0/CurrencyRates.Persistence.AssemblyInfo.cs
+++ src/CurrencyRates.Persistence/obj/Debug/net8.0/CurrencyRates.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyRates.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fc1b9ab7d18b2e4f4a862c81f33222fbb6ab68")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyRates.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyRates.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
